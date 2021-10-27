2371b99bfb212dd23afaed413a033512a8c1d08d
folka2134 <folka2134@gmail.com>
2021-10-27 13:07:47 +0100

    Created cards with front and back images, implemented shuffle function to shuffle order of cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
-function App() {
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
+
+const App = () => {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Snap Memory</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map((card) => (
+          <div key={card.id} className="card">
+            <div>
+              <img alt="card front" src={card.src} className="card-front" />
+              <img alt="card back" src="/img/cover.png" className="card-back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
